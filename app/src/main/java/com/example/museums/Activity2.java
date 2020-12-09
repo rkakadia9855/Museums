d2d3371fcf9471b555ac78915443c8e0ee054c59
rkakadia9855 <rudrakakadia9855@gmail.com>
2020-12-09 22:30:16 +0530

    added 2nd activity

---
+++ app/src/main/java/com/example/museums/Activity2.java
@@ -0,0 +1,17 @@
+package com.example.museums;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+    }
+}
