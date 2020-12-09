b103204a657a02070c1a5621d279ef4729657de5
rkakadia9855 <rudrakakadia9855@gmail.com>
2020-12-09 23:48:54 +0530

    edited manifest file to make second activity open up

--- app/src/main/java/com/example/museums/Activity2.java
+++ app/src/main/java/com/example/museums/Activity2.java
@@ -10,39 +10,39 @@ import org.w3c.dom.Text;
 
 public class Activity2 extends AppCompatActivity {
 
-    TextView textView;
-    double adultPrice = 0.0;
-    double seniorPrice = 0.0;
-    double studentPrice = 0.0;
+    TextView title;
+    String adultPrice = "";
+    String seniorPrice = "";
+    String studentPrice = "";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        textView = (TextView) findViewById(R.id.textView);
+        title = (TextView) findViewById(R.id.textView);
         String selectedMuseum = getIntent().getStringExtra("Listviewclickvalue");
-        textView.setText(selectedMuseum);
+        title.setText(selectedMuseum);
 
         if(selectedMuseum.equals("The Museum of Modern Art")) {
-            adultPrice = 25.00;
-            seniorPrice = 18.00;
-            studentPrice = 14.00;
+            adultPrice = "adult $25.00";
+            seniorPrice = "senior $18.00";
+            studentPrice = "student $14.00";
         }
         else if(selectedMuseum.equals("Rubin Museum of Art")) {
-            adultPrice = 19.00;
-            seniorPrice = 14.00;
-            studentPrice = 14.00;
+            adultPrice = "adult $19.00";
+            seniorPrice = "senior $14.00";
+            studentPrice = "student $14.00";
         }
         else if(selectedMuseum.equals("American Museum of Natural History")) {
-            adultPrice = 23.00;
-            seniorPrice = 18.00;
-            studentPrice = 18.00;
+            adultPrice = "adult $23.00";
+            seniorPrice = "senior $18.00";
+            studentPrice = "student $18.00";
         }
         else if(selectedMuseum.equals("Whitney Museum of American Art")) {
-            adultPrice = 25.00;
-            seniorPrice = 18.00;
-            studentPrice = 18.00;
+            adultPrice = "adult $25.00";
+            seniorPrice = "senior $18.00";
+            studentPrice = "student $18.00";
         }
 
     }
