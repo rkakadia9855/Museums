5a075decc8f11524b340808b41cd3bd351b48bbe
rkakadia9855 <rudrakakadia9855@gmail.com>
2020-12-09 22:24:22 +0530

    initial files

---
+++ app/src/main/java/com/example/museums/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.museums;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("The New York Hall of Science");
+        list.add("Museum of Modern Art");
+        list.add("Intrepid Sea, Air, and Space Museum");
+        list.add("Whitney Museum of American Art");
+
+        ArrayAdapter arr = new ArrayAdapter(this, android.R.layout.simple_list_item_1,list);
+
+        listView.setAdapter(arr);
+    }
+
+}
