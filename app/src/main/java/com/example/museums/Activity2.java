f32a869bc0b3c341a44b8515ac732e3a6276213c
rkakadia9855 <rudrakakadia9855@gmail.com>
2020-12-09 23:33:12 +0530

    added prices for different museums

--- app/src/main/java/com/example/museums/Activity2.java
+++ app/src/main/java/com/example/museums/Activity2.java
@@ -11,6 +11,10 @@ import org.w3c.dom.Text;
 public class Activity2 extends AppCompatActivity {
 
     TextView textView;
+    double adultPrice = 0.0;
+    double seniorPrice = 0.0;
+    double studentPrice = 0.0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,27 @@ public class Activity2 extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         String selectedMuseum = getIntent().getStringExtra("Listviewclickvalue");
         textView.setText(selectedMuseum);
+
+        if(selectedMuseum.equals("The Museum of Modern Art")) {
+            adultPrice = 25.00;
+            seniorPrice = 18.00;
+            studentPrice = 14.00;
+        }
+        else if(selectedMuseum.equals("Rubin Museum of Art")) {
+            adultPrice = 19.00;
+            seniorPrice = 14.00;
+            studentPrice = 14.00;
+        }
+        else if(selectedMuseum.equals("American Museum of Natural History")) {
+            adultPrice = 23.00;
+            seniorPrice = 18.00;
+            studentPrice = 18.00;
+        }
+        else if(selectedMuseum.equals("Whitney Museum of American Art")) {
+            adultPrice = 25.00;
+            seniorPrice = 18.00;
+            studentPrice = 18.00;
+        }
 
     }
 }
